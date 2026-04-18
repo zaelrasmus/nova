8b370d25185ba172eed915af0553087c5678e7b2
zaelrasmus <zaelfox22@gmail.com>
2026-04-18 07:47:53 -0400

    feat(backend): implement config for tracing

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -7,6 +7,17 @@ mod library;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| "nova=debug,sqlx=warn,tauri=warn,tao=warn".into()),
+        )
+        .with_target(false)
+        .compact()
+        .init();
+
+    tracing::info!("Starting Nova Application");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
         .manage(db::DbState::new())
